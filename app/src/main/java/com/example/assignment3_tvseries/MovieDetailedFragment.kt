1a207af6d50552a18d425b5b1b689b0c42dd4492
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-12-08 15:57:07 +0400

    grgggeg

--- app/src/main/java/com/example/assignment3_tvseries/MovieDetailedFragment.kt
+++ app/src/main/java/com/example/assignment3_tvseries/MovieDetailedFragment.kt
@@ -11,15 +11,7 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
-
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
-    }
+class MovieDetailedFragment : Fragment(R.layout.fragment_movie_detailed) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/assignment3_tvseries/MovieFragment.kt
+++ app/src/main/java/com/example/assignment3_tvseries/MovieFragment.kt
@@ -11,21 +11,13 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
-
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+class MovieFragment : Fragment(R.layout.fragment_movie) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_MovieFragment_to_MovieDetailedFragment)
         }
     }
 }
